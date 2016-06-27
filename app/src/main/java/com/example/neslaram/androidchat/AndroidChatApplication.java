ad2b8ce439fea0ed1f23bef4b9da9a40691a0ba8
laran <nestor.lara@elnorte.com>
2016-06-27 15:29:51 -0500

    ChatApplication class added

---
+++ app/src/main/java/com/example/neslaram/androidchat/AndroidChatApplication.java
@@ -0,0 +1,29 @@
+package com.example.neslaram.androidchat;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class AndroidChatApplication extends Application {
+//    private ImageLoader imageLoader;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setupFirebase();
+        setupImageLoader();
+    }
+
+    private void setupImageLoader() {
+//        imageLoader = new GlideImageLoader(this);
+    }
+
+//    public ImageLoader getImageLoader() {
+//        return imageLoader;
+//    }
+
+    private void setupFirebase(){
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+}
